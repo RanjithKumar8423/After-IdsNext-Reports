264e64446336c1b94376731e2fb43cc93641f581
Ranjith Kumar N <ranjith.kumar@azidsad.local>
2026-01-18 18:28:14 +0530

    docker changes

--- src/main/java/com/qa/reports/factory/DriverFactory.java
+++ src/main/java/com/qa/reports/factory/DriverFactory.java
@@ -2,11 +2,14 @@ package com.qa.reports.factory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.qa.reports.expections.BrowserExpection;
 import com.qa.reports.expections.ElementExpection;
@@ -16,7 +19,7 @@ public class DriverFactory {
 	WebDriver driver;
 	Properties prop;
 	OptionsManager optionsManager;
-	public static ThreadLocal<WebDriver> tLDriver=new ThreadLocal<WebDriver>();
+	public static ThreadLocal<WebDriver> tLDriver = new ThreadLocal<WebDriver>();
 
 	public WebDriver initDriver(Properties prop) {
 		optionsManager = new OptionsManager(prop);
@@ -24,13 +27,27 @@ public class DriverFactory {
 
 		switch (browserName.trim().toLowerCase()) {
 		case "chrome":
-			//driver = new ChromeDriver(optionsManager.getChromeOptions());
-			tLDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
-			break;
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				// remote - grid execution:
+				init_remoteDriver("chrome");
+			}
+
+			else {
+				// driver = new ChromeDriver(optionsManager.getChromeOptions());
+				tLDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+				break;
+			}
+
 		case "edge":
-			//driver = new EdgeDriver(optionsManager.getEdgeOptions());
-			tLDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
-			break;
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				// remote - grid execution:
+				init_remoteDriver("edge");
+			} else {
+
+				// driver = new EdgeDriver(optionsManager.getEdgeOptions());
+				tLDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
+				break;
+			}
 
 		default:
 			System.out.println("=======PLS PASS RIGHT BROWSER========");
@@ -43,10 +60,39 @@ public class DriverFactory {
 		return getDriver();
 
 	}
-	public static WebDriver getDriver() {
-		 return tLDriver.get();
-		 }
 
+	private void init_remoteDriver(String browserName) {
+
+		System.out.println("Running tests on Remote GRID on browser: " + browserName);
+
+		try {
+			switch (browserName.toLowerCase().trim()) {
+			case "chrome":
+
+				tLDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getChromeOptions()));
+				break;
+
+			case "edge":
+				tLDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getEdgeOptions()));
+				break;
+
+			default:
+				System.out.println("plz pass thr right supported browser on GRID....");
+				break;
+			}
+
+		}
+
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static WebDriver getDriver() {
+		return tLDriver.get();
+	}
 
 	public Properties intiprop() {
 		prop = new Properties();
--- src/main/java/com/qa/reports/factory/OptionsManager.java
+++ src/main/java/com/qa/reports/factory/OptionsManager.java
@@ -4,64 +4,74 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 
 public class OptionsManager {
-	
-	 private Properties prop;
 
-	 private ChromeOptions co;
-	 private EdgeOptions eo;
-	 
-	 public OptionsManager(Properties prop) {
-		 this.prop=prop;
-	 }
-	 public ChromeOptions getChromeOptions() { 
-		 co=new ChromeOptions();
-		 
+	private Properties prop;
+
+	private ChromeOptions co;
+	private EdgeOptions eo;
+
+	public OptionsManager(Properties prop) {
+		this.prop = prop;
+	}
+
+	public ChromeOptions getChromeOptions() {
+		co = new ChromeOptions();
+
 		// ===== Disable Chrome Password & Security Popups =====
-			Map<String, Object> prefs = new HashMap<>();
-			prefs.put("credentials_enable_service", false);
-			prefs.put("profile.password_manager_enabled", false);
-			prefs.put("profile.password_manager_leak_detection", false);
+		Map<String, Object> prefs = new HashMap<>();
+		prefs.put("credentials_enable_service", false);
+		prefs.put("profile.password_manager_enabled", false);
+		prefs.put("profile.password_manager_leak_detection", false);
 
-			co.setExperimentalOption("prefs", prefs);
+		co.setExperimentalOption("prefs", prefs);
 
-			// Disable automation info bar & notifications
-			co.addArguments("--disable-notifications");
-			co.addArguments("--disable-infobars");
-			co.addArguments("--disable-save-password-bubble");
-			co.addArguments("--disable-features=PasswordLeakDetection");
-			co.addArguments("--disable-features=AutofillServerCommunication");
-		 
-		 
-		 
-		 if(Boolean.parseBoolean(prop.getProperty("headless"))) {
-			 System.out.println("======RUNNING ON HEADLESS MODE=====");
-			 co.addArguments("--headless");
-		 }
-		 if (Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
-			   //Log.info("Running chrome in incognito mode");
-			   co.addArguments("--incognito");
-			  }
-		 return co;
+		// Disable automation info bar & notifications
+		co.addArguments("--disable-notifications");
+		co.addArguments("--disable-infobars");
+		co.addArguments("--disable-save-password-bubble");
+		co.addArguments("--disable-features=PasswordLeakDetection");
+		co.addArguments("--disable-features=AutofillServerCommunication");
 
-	 }
-	 public EdgeOptions getEdgeOptions() { 
-		 eo=new EdgeOptions();
-		 if(Boolean.parseBoolean(prop.getProperty("headless"))) {
-			 System.out.println("======RUNNING ON HEADLESS MODE=====");
-			 eo.addArguments("--headless");
-		 }
-		 if (Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
-			   //Log.info("Running chrome in incognito mode");
-			   eo.addArguments("--inprivate");
-			  }
-		 return eo;
+		if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+			co.setCapability("browserName", "chrome");
 
-	 }
+		}
 
-	
-	
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println("======RUNNING ON HEADLESS MODE=====");
+			co.addArguments("--headless");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
+			// Log.info("Running chrome in incognito mode");
+			co.addArguments("--incognito");
+		}
+		return co;
+
+	}
+
+	public EdgeOptions getEdgeOptions() {
+		eo = new EdgeOptions();
+		if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+			eo.setCapability("browserName", "edge");
+			eo.setCapability("platform", Platform.LINUX);
+
+		}
+
+		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
+			System.out.println("======RUNNING ON HEADLESS MODE=====");
+			eo.addArguments("--headless");
+		}
+		if (Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
+			// Log.info("Running chrome in incognito mode");
+			eo.addArguments("--inprivate");
+		}
+		return eo;
+
+	}
+
 }
